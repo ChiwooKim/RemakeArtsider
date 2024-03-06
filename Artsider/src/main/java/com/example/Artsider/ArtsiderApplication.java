b440353f8f180a01928c67cf325e44cb26c08834
kcw0360 <kcw0360@naver.com>
2024-03-06 17:46:14 +0900

    프로젝트 생성

---
+++ Artsider/src/main/java/com/example/Artsider/ArtsiderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Artsider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtsiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtsiderApplication.class, args);
+	}
+
+}
